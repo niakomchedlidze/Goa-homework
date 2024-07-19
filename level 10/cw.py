9884a75cd411ea94a5a91597d808b1fb19a0c4bf
niakomchedlidze <niako.mchedlidze1@gmail.com>
2024-07-19 14:00:40 +0400

    second commit

---
+++ level 10/cw.py
@@ -0,0 +1,1 @@
+intend("lemur like manggo")
---
+++ level 10/hw.py
@@ -0,0 +1,18 @@
+name = "niako"
+surname = "mchedlidze"
+print("name + surname")
+
+name = "gvanca"
+surname = "gabiani"
+gvanca ="happy birthday"
+print("gvanca")
+
+pet ="cat"
+color = "red"
+car = "bmw"
+actor = "bred pit"
+print("pet + color + car + actor")
+
+# input ინპუტ-ი არის მაგალითად ჩვენ რამეს რომ ვწერთ ან რამეს რომ ვაჭერთ ხელსს
+# autput აუთპუტი არის რასაც გამოგვიტანს ეკრანი
+#concatenation არის სტრინგების შეერთება
